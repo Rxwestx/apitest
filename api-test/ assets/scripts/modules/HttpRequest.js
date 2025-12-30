41d6f98ce2234b7c0eb2c2203a73207624281c27
Rxwestx <wexriexst13@gmail.com>
2025-12-30 23:30:22 +0900

    Modified_the_file

--- api-test/ assets/scripts/modules/HttpRequest.js
+++ api-test/ assets/scripts/modules/HttpRequest.js
@@ -1,12 +1,12 @@
-import axios, from 'axios';
+import axios from 'axios';
 
-const  instance  =  axios.create ( {
+const  instance  =  axios.create ({
      baseURL : 'https://pokeapi.co/api/v2/pokemon/', 
      timeout:1000 
-    } ) ;
+    });
 
     // instance
-    export  const getPokemonData =async ( pokemonName )  =>  {
+    export  const getPokemonData = async ( pokemonName )  =>  {
         try {
             const response = await instance.get(pokemonName);
             return response.data;
